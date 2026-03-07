9a4c93a4853fc2d3a25a7ae540756b549882c924
Emil <emil.galiev.a@gmail.com>
2026-03-07 14:30:52 +0300

    solution

--- main.py
+++ main.py
@@ -1,41 +1,50 @@
-from flask import Flask, url_for
+import flask
+from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def main_page():
-    return "Миссия Колонизация Марса"
+@app.route("/carousel")
+def info():
+    return f"""
+        <!DOCTYPE html>
+        <html lang="ru">
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                <link rel="stylesheet"
+                                href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                                integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                                crossorigin="anonymous">
+                
+                <link rel="stylesheet" href="{flask.url_for("static", filename="css/default.css")}">
+                <title>Пейзажи марса</title>
+            </head>
+            <body>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+                 integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+                 crossorigin="anonymous"></script>
+                <h1>Пейзажи марса</h1>
+                <div id="carouselWithControls" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div style="text-align: center;" class="carousel-item active" data-bs-interval="2000">
+                            <img src="{flask.url_for("static", filename="img/1.jpg").removeprefix("/")}"
+                             width="400" height="400">
+                        </div>
+                        <div style="text-align: center;" class="carousel-item" data-bs-interval="2000">
+                            <img src="{flask.url_for("static", filename="img/2.jpg").removeprefix("/")}"
+                             width="400" height="400">
+                        </div>
+                        <div style="text-align: center;" class="carousel-item" data-bs-interval="2000">
+                            <img src="{flask.url_for("static", filename="img/3.jpg").removeprefix("/")}"
+                             width="400" height="400">
+                        </div>
+                    </div>
+                </div>
+            </body>
+        </html>
+    """
 
 
-@app.route('/index')
-def index():
-    return "И на Марсе будут яблони цвести!"
-
-@app.route("/promotion")
-def promotion():
-    return """Человечество вырастает из детства.<br>
-            Человечеству мала одна планета.<br>
-            Мы сделаем обитаемыми безжизненные пока планеты.<br>
-            И начнем с Марса!<br>
-            Присоединяйся!"""
-
-
-@app.route("/image_mars")
-def image_mars():
-    return f"""<!doctype html>
-                <html lang="ru">
-                  <head>
-                    <meta charset="utf-8">
-                    <title>Привет, Марс!</title>
-                  </head>
-                  <body>
-                    <h1>Жди нас, Марс!</h1>
-                    <img src="{url_for('static', filename='images/MARS.png')}"><br>
-                    Вот она какая, красная планета.
-                  </body>
-                </html>"""
-
-
-if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+if __name__ == "__main__":
+    app.run("127.0.0.1", 8080)
